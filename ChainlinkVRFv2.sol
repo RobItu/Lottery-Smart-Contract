4dba014910ba1ecf4b2db125806cee387bd75b12
RobItu <74835755+RobItu@users.noreply.github.com>
2022-08-14 13:50:47 -0600

    Create ChainlinkVRFv2.sol

---
+++ ChainlinkVRFv2.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "./Lottery.sol";
+
+contract VRFv2Consumer is VRFConsumerBaseV2 {
+  VRFCoordinatorV2Interface COORDINATOR;
+
+  // Your subscription ID.
+  uint64 s_subscriptionId;
+
+  // Rinkeby coordinator.
+  address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+
+  // The gas lane to use, which specifies the maximum gas price to bump to.
+  bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+  uint32 callbackGasLimit = 100000;
+
+  // The default is 3, but you can set this higher.
+  uint16 requestConfirmations = 3;
+
+  // Retrieve 1 random value in one request.
+
+  uint32 numWords =  2;
+
+  uint256[] public s_randomWords; // Random words list
+  uint256 public  s_requestId;
+  address s_owner;
+
+  constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    s_owner = msg.sender;
+    s_subscriptionId = subscriptionId;
+  }
+
+  // Assumes the subscription is funded sufficiently.
+  function requestRandomWords() external onlyOwner {
+    // Will revert if subscription is not set and funded.
+    s_requestId = COORDINATOR.requestRandomWords(
+      keyHash,
+      s_subscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      numWords
+    );
+  }
+
+  function fulfillRandomWords(
+    uint256, /* requestId */
+    uint256[] memory randomWords
+  ) internal override {
+    s_randomWords = randomWords;
+  }
+
+  function sendNumber() external view returns(uint256[] memory) {
+    return s_randomWords;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == s_owner);
+    _;
+  }
+}
