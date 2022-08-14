4a78a7c2ff5457a0ec5573fd5dba17eae4be0a91
RobItu <74835755+RobItu@users.noreply.github.com>
2022-08-14 13:52:14 -0600

    Create Payments.sol

---
+++ Payments.sol
@@ -0,0 +1,10 @@
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
+
+// Contract that will split the shares 50, 50 to winner and owner.
+contract Payments is PaymentSplitter{
+    constructor(address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares)  payable{}
+}
